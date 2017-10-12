b46e716dad2b8fe7561d2543e319de5699464d2e
valentin <valen.da.974@gmail.com>
2017-10-12 15:42:02 +0400

    Début activité des scores

--- app/src/main/java/com/example/valentin/gameproject/MainActivity.java
+++ app/src/main/java/com/example/valentin/gameproject/MainActivity.java
@@ -5,14 +5,20 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    public Button play;
+    public Button score;
+    private boolean inPlay=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        play=(Button)findViewById(R.id.btnPlay);
+        score=(Button)findViewById(R.id.btnScore);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -30,5 +36,11 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void playPush(View v){
+        inPlay=true;
+        play.setVisibility(View.GONE);
+        score.setVisibility(View.GONE);
     }
 }
---
+++ app/src/main/java/com/example/valentin/gameproject/ScoreActivity.java
@@ -0,0 +1,8 @@
+package com.example.valentin.gameproject;
+
+/**
+ * Created by valentin on 12/10/17.
+ */
+
+public class ScoreActivity {
+}
