b24b10425b8cb8d68d3168cbc5e6175e443f4422
valentin <valen.da.974@gmail.com>
2017-10-12 14:52:49 +0400

    Première

--- app/src/main/java/com/example/valentin/gameproject/MainActivity.java
+++ app/src/main/java/com/example/valentin/gameproject/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.valentin.gameproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.score:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
