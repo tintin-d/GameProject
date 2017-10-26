d13452b97e90972509d0216e2333b77f9c20f5da
valentin <valen.da.974@gmail.com>
2017-10-26 14:10:01 +0400

    Début implémentation mouvements

--- app/src/main/java/com/example/valentin/gameproject/Game_activity.java
+++ app/src/main/java/com/example/valentin/gameproject/Game_activity.java
@@ -1,6 +1,11 @@
 package com.example.valentin.gameproject;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import java.nio.Buffer;
@@ -9,11 +14,70 @@ import java.nio.Buffer;
  * Created by valentin on 12/10/17.
  */
 
-public class Game_activity extends Activity {
+public class Game_activity extends Activity implements SensorEventListener{
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private boolean accelSopported;
+    private Game_view gameView;
+    private boolean started=false;
+    private float speedX;
+    private float speedY;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_layout);
+        gameView=(Game_view)findViewById(R.id.game_view);
+        mSensorManager=(SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer=mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        accelSopported=mSensorManager.registerListener(this,mAccelerometer,SensorManager.SENSOR_DELAY_FASTEST);
+        speedX=speedY=1;
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        switch (sensorEvent.sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER:
+                float ax = sensorEvent.values[0];
+                float ay = sensorEvent.values[1];
+                float az = sensorEvent.values[2];
+                double xAngle = Math.atan( ax / (Math.sqrt((ay*ay) + (az*az))));
+               // double yAngle = Math.atan( ay / (Math.sqrt((ax*ax) + (az*az))));
+
+                xAngle *= 180.00;   //yAngle *= 180.00;
+                xAngle /= 3.141592; //yAngle /= 3.141592;
+                speedX=-(float)(xAngle/10);
+                //speedY=(float)(yAngle/10);
+                move();
+                break;
+            default:
+                break;
+    }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onPause(){
+        mSensorManager.unregisterListener(this, mAccelerometer);
+        super.onPause();
+    }
+
+
+    public void decompte(){
+    }
+
+    public void move(){
+        float curX=gameView.getMyX();
+        //float curY=gameView.getMyY();
+        gameView.setMyX((curX+speedX));
+       // gameView.setMyY((curY+speedY*1));
+        gameView.invalidate();
+    }
+
 }
--- app/src/main/java/com/example/valentin/gameproject/Game_view.java
+++ app/src/main/java/com/example/valentin/gameproject/Game_view.java
@@ -1,8 +1,88 @@
 package com.example.valentin.gameproject;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Gallery;
+import android.widget.ImageView;
+
 /**
  * Created by valentin on 12/10/17.
  */
 
-public class Game_view {
+public class Game_view extends View {
+
+    Paint paint;
+    private Drawable mCustomImage;
+    float x, y;
+    boolean first=true;
+    public Game_view(Context context) {
+        super(context);
+    }
+
+    public Game_view(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        mCustomImage = context.getResources().getDrawable(R.drawable.cannon);
+    }
+
+    public Game_view(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mCustomImage = context.getResources().getDrawable(R.drawable.cannon);
+    }
+
+
+
+    @Override
+    public  void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        if(first){
+            //première apparition du cannon
+            x=canvas.getWidth()/2;
+            y=canvas.getHeight()-100;
+            first=false;
+        }
+        int maxW=canvas.getWidth();
+        int maxH=canvas.getHeight();
+        //Bornes de l'écran
+        if(x>=maxW-70){x=maxW-70;}
+        if(x<=20){x=0;}
+        if(y>=maxH-20){y=maxH-20;}
+        if(y<=20){y=20;}
+
+
+        paint= new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.FILL);
+
+        mCustomImage.setBounds((int)x,(int)y,(int)x+70,(int)y+80);
+        mCustomImage.draw(canvas);
+    }
+
+
+    public float getMyX() {
+        return x;
+    }
+
+    public float getMyY() {
+        return y;
+    }
+
+    public void setMyX(float x) {
+        this.x = x;
+    }
+
+    public void setMyY(float y) {
+        this.y = y;
+    }
 }
